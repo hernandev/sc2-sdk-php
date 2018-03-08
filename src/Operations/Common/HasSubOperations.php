8113a082233e91b2c73ba833abea2a88d073a694
Diego Hernandes <diego@hernandev.com>
2018-03-08 11:46:55 -0300

    json serialization

---
+++ src/Operations/Common/HasSubOperations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SteemConnect\Operations\Common;
+
+/**
+ * Trait HasSubOperations.
+ *
+ * This trait empowers custom_json operations allowing easy
+ */
+trait HasSubOperations
+{
+
+}
