ed9678acbf1f2d4b64dafe50ab0c0bed82a16594
Diego Hernandes <diego@hernandev.com>
2018-03-08 11:45:20 -0300

    config and client exceptions

---
+++ src/Exceptions/ClientException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SteemConnect\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class ClientException.
+ *
+ * Base SteemConnect V2 SDK exception.
+ */
+class ClientException extends RuntimeException
+{
+    //
+}
