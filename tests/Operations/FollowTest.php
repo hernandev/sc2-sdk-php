0b6fed374797385e08f5e7048c4631b076f0cd93
Diego Hernandes <diego@hernandev.com>
2018-04-08 04:52:31 -0300

    update follow test to cover more custom_json operation methods

--- tests/Operations/FollowTest.php
+++ tests/Operations/FollowTest.php
@@ -53,4 +53,86 @@ class FollowTest extends TestCase
 
         $follow->following('other', true);
     }
+
+    /**
+     * Test the getter on a sub operation property.
+     */
+    public function test_defined_sub_operation_property()
+    {
+        // start a follow operation.
+        $follow = new Follow();
+
+        // use the magic setter against a already defined property.
+        $this->assertNotNull($follow->getSubOperation()->parameters);
+    }
+
+    /**
+     * Required auths and posting auths testing.
+     */
+    public function test_required_auths()
+    {
+        // start a follow operation.
+        $follow = new Follow();
+
+        // required auths getter / setter tests.
+        $this->assertEquals([], $follow->getRequiredAuths());
+        $this->assertEquals([], $follow->getRequiredPostingAuths());
+
+        // custom the auths.
+        $follow->setRequiredAuths(['a', 'b']);
+        $follow->setRequiredPostingAuths(['c', 'd']);
+
+        // required auths getter tests.
+        $this->assertEquals(['a', 'b'], $follow->getRequiredAuths());
+        $this->assertEquals(['c', 'd'], $follow->getRequiredPostingAuths());
+    }
+
+    /**
+     * Sub operation testing.
+     */
+    public function test_sub_operations()
+    {
+        // start a follow operation.
+        $follow = new Follow();
+
+        // get the sub operation (currently empty);
+        $subOperation = $follow->getSubOperation();
+        // set a given parameter/
+        $subOperation->setParameter('foo', 'bar');
+
+        // set the sub operation back.
+        $follow->setSubOperation($subOperation);
+
+        // call the magic getter to extract the value.
+        $this->assertEquals('bar', $follow->foo);
+    }
+
+    /**
+     * Test direct parameters getter.
+     */
+    public function test_magic_getter_against_local_property()
+    {
+        // start a follow operation.
+        $follow = new Follow();
+
+        // test the getter and it's value.
+        $this->assertEquals('follow', $follow->customOperation);
+    }
+
+    /**
+     * Assert parsing JSON string on the constructor.
+     */
+    public function test_json_string_parsing_on_constructor()
+    {
+        // dummy operation data.
+        $operation = [
+            'json' => json_encode(['foo' => 'bar'])
+        ];
+
+        // start a follow instance, passing some data.
+        $follow = new Follow($operation);
+
+        // assert the value.
+        $this->assertEquals('bar', $follow->foo);
+    }
 }
